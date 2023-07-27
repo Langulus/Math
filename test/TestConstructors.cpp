4838289a053744e253d84b4c2d8355a1d2d916f5
Dimo Markov <dimomarkov@gmail.com>
2023-07-28 01:54:23 +0300

    Tests improved

---
+++ test/TestConstructors.cpp
@@ -0,0 +1,44 @@
+﻿#include "Main.hpp"
+#include <Math/Vectors.hpp>
+#include <catch2/catch.hpp>
+
+LANGULUS_EXCEPTION_HANDLER
+
+#if LANGULUS_FEATURE(MEMORY_STATISTICS)
+static bool statistics_provided = false;
+static Anyness::Allocator::Statistics memory_statistics;
+#endif
+
+TEMPLATE_TEST_CASE("Vector static creation from code", "[vec]",
+   Vec1, Vec2, Vec3, Vec4
+) {
+   GIVEN("Default construction") {
+      const Code code = Code {NameOf<TestType>()} + "()";
+      
+      WHEN("Parsed") {
+         Any required = TestType {};
+
+         const auto parsed = code.Parse();
+         DumpResults(code, parsed, required);
+         THEN("The parsed contents must match the requirements") {
+            REQUIRE(parsed == required);
+         }
+      }
+      
+      #if LANGULUS_FEATURE(MEMORY_STATISTICS)
+         Fractalloc.CollectGarbage();
+
+         // Detect memory leaks                                         
+         if (statistics_provided) {
+            if (memory_statistics != Fractalloc.GetStatistics()) {
+               Fractalloc.DumpPools();
+               memory_statistics = Fractalloc.GetStatistics();
+               FAIL("Memory leak detected");
+            }
+         }
+
+         memory_statistics = Fractalloc.GetStatistics();
+         statistics_provided = true;
+      #endif
+   }
+}
--- test/TestRTTI.cpp
+++ test/TestRTTI.cpp
@@ -3,11 +3,7 @@
 #include <Math/Colors.hpp>
 #include <catch2/catch.hpp>
 
-/// See https://github.com/catchorg/Catch2/blob/devel/docs/tostring.md        
-CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
-   const Text serialized {ex};
-   return ::std::string {Token {serialized}};
-}
+LANGULUS_EXCEPTION_HANDLER
 
 SCENARIO("Testing vector names", "[rtti]") {
    GIVEN("All vector types") {
